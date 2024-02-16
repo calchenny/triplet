698a59306ed6cbc08b892e30092e0e1e25850682
Derek Ma <danderekma@gmail.com>
2024-02-15 23:45:41 -0800

    Init swift project

---
+++ Triplet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Triplet
+//
+//  Created by Derek Ma on 2/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Triplet/TripletApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripletApp.swift
+//  Triplet
+//
+//  Created by Derek Ma on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TripletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
