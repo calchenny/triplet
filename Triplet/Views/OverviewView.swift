4a40ffdb20d45073f90b55984d7773a7c5805fb5
Derek Ma <danderekma@gmail.com>
2024-02-20 14:02:31 -0800

    Install header package

---
+++ Triplet/Views/OverviewView.swift
@@ -0,0 +1,25 @@
+//
+//  OverviewView.swift
+//  Triplet
+//
+//  Created by Derek Ma on 2/20/24.
+//
+
+import SwiftUI
+
+struct OverviewView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    OverviewView()
+}
+
