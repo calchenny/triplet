8b0baf984b6e67bab05d796f2e9a3e542ba0faa2
Derek Ma <danderekma@gmail.com>
2024-02-27 19:55:19 -0800

    Add food category dropdown

--- Triplet/Views/FoodPopupView.swift
+++ Triplet/Views/FoodPopupView.swift
@@ -7,8 +7,15 @@
 
 import SwiftUI
 
+enum FoodCategory: String, CaseIterable {
+    case breakfast = "Breakfast/Brunch"
+    case lunch = "Lunch"
+    case dinner = "Dinner"
+}
+
 struct FoodPopupView: View {
     @EnvironmentObject var overviewViewModel: OverviewViewModel
+    @State var selection: FoodCategory = .breakfast
     
     var body: some View {
         RoundedRectangle(cornerRadius: 15)
@@ -25,8 +32,28 @@ struct FoodPopupView: View {
                         }
                     }
                     Text("New Food Spot")
-                        .font(.system(size: 30, weight: .bold))
+                        .font(.title)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                         .foregroundStyle(.indigo)
+                    Spacer()
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Category")
+                                .font(.title3)
+                            Picker("Select a paint color", selection: $selection) {
+                                ForEach(FoodCategory.allCases, id: \.self) { category in
+                                    Text(category.rawValue)
+                                }
+                            }
+                            .frame(maxWidth: 200)
+                            .pickerStyle(.menu)
+                            .tint(.indigo)
+                            .padding(5)
+                            .overlay(RoundedRectangle(cornerRadius: 5).stroke(.gray))
+                        }
+                        Spacer()
+                    }
+                    .padding()
                     Spacer()
                     Button {
                         
