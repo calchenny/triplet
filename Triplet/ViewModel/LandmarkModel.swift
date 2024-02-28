3932a602e41f21cc335336395c14f25d9c06f6aa
Andy Lam <aandylam21@gmail.com>
2024-02-28 09:42:21 -0800

    landmark model

---
+++ Triplet/ViewModel/LandmarkModel.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkModel.swift
+//  Triplet
+//
+//  Created by Andy Lam on 2/28/24.
+//
+
+import Foundation
+import MapKit
+
+struct LandmarkModel: Identifiable, Hashable{
+    let placemark: MKPlacemark
+    var id: UUID {
+        return UUID()
+    }
+    
+    var name: String {
+        self.placemark.name ?? ""
+    }
+    
+    var title: String {
+        self.placemark.title ?? ""
+    }
+    
+    var coordinate: CLLocationCoordinate2D {
+        self.placemark.coordinate
+    }
+}
+
