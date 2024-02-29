197428733e32e44f53a98e1c4414ea49f807f668
Derek Ma <danderekma@gmail.com>
2024-02-28 17:05:41 -0800

    Add trip structs

--- Triplet/Models/TripModel.swift
+++ Triplet/Models/TripModel.swift
@@ -12,15 +12,47 @@ struct Trip: Identifiable, Codable {
     @DocumentID var id: String?
     var owner: String
     var name: String
-    var start: Date
-    var end: Date
+    var start: Date?
+    var end: Date?
     var destination: GeoPoint
     var numGuests: Int?
     var notes: [Note]
+    var events: [Event]
+    var expenses: [Expense]
 }
 
 struct Note: Identifiable, Codable {
-    var id = UUID()
+    var id: String?
     var title: String = "Untitled Note"
     var content: String = ""
+}
+
+enum EventType: String, Codable {
+    case housing = "Housing"
+    case food = "Food"
+    case attraction = "Attraction"
+}
+
+enum FoodCategory: String, Codable, CaseIterable {
+    case breakfast = "Breakfast/Brunch"
+    case lunch = "Lunch"
+    case dinner = "Dinner"
+}
+
+struct Event: Identifiable, Codable {
+    var id: String?
+    var name: String
+    var location: GeoPoint
+    var type: EventType
+    var category: FoodCategory?
+    var start: Date?
+    var end: Date?
+}
+
+struct Expense: Identifiable, Codable {
+    var id: String?
+    var name: String
+    var date: Date?
+    var category: String?
+    var cost: Double
 }
