d14caa7fa703a4846623070534c36d91d361b211
Calvin Chen <calvinchen345@gmail.com>
2024-03-01 00:21:25 -0800

    Add interactable map view with important locations pinned

---
+++ Triplet/ViewModels/UserLocationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserLocationViewModel.swift
+//  Triplet
+//
+//  Created by Calvin Chen on 2/29/24.
+//
+
+import Foundation
---
+++ Triplet/Views/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Triplet
+//
+//  Created by Calvin Chen on 2/29/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
