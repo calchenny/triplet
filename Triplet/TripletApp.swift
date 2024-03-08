751e780acda8594b5d9216efe202b54374910785
Derek Ma <danderekma@gmail.com>
2024-03-07 20:35:39 -0800

    Change Launch Screen icon

--- Triplet/TripletApp.swift
+++ Triplet/TripletApp.swift
@@ -9,16 +9,10 @@ import SwiftUI
 import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
     return true
   }
-    
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        
-    }
-    
 }
 
 @main
